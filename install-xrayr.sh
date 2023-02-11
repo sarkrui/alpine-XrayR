22ef93895f5be6896b052e1b8bdf43d8ca921638
Sark <sark@Systemss-Mac-mini-2.local>
2023-02-11 14:03:14 +0800

    initial commit

--- install-xrayr.sh
+++ install-xrayr.sh
@@ -80,11 +80,6 @@ download_xrayr() {
         echo 'error: Download failed! Please check your network or try again.'
         exit 1
     fi
-    curl -L -H 'Cache-Control: no-cache' -o "$ZIP_FILE.dgst" "$DOWNLOAD_LINK.dgst" -#
-    if [ "$?" -ne '0' ]; then
-        echo 'error: Download failed! Please check your network or try again.'
-        exit 1
-    fi
 }
 
 decompression() {
@@ -101,7 +96,7 @@ is_it_running() {
 }
 
 install_xrayr() {
-    install -m 755 "${TMP_DIRECTORY}xray" "/usr/local/XrayR/XrayR"
+    install -m 755 "${TMP_DIRECTORY}XrayR" "/usr/local/XrayR/XrayR"
     install -d /usr/local/XrayR/
     install -m 755 "${TMP_DIRECTORY}geoip.dat" "/etc/XrayR/geoip.dat"
     install -m 755 "${TMP_DIRECTORY}geosite.dat" "/etc/XrayR/geosite.dat"
@@ -109,10 +104,10 @@ install_xrayr() {
 
 install_confdir() {
     CONFDIR='0'
-    if [ ! -d '/usr/local/etc/xray/' ]; then
-        install -d /usr/local/etc/xray/
+    if [ ! -d '/usr/local/etc/XrayR/' ]; then
+        install -d /usr/local/etc/XrayR/
         for BASE in 00_log 01_api 02_dns 03_routing 04_policy 05_inbounds 06_outbounds 07_transport 08_stats 09_reverse; do
-            echo '{}' > "/usr/local/etc/xray/$BASE.json"
+            echo '{}' > "/usr/local/etc/XrayR/$BASE.json"
         done
         CONFDIR='1'
     fi
@@ -140,32 +135,32 @@ information() {
     echo 'installed: /etc/XrayR/geosite.dat'
     echo 'installed: /etc/XrayR/config.yml'
     if [ "$CONFDIR" -eq '1' ]; then
-        echo 'installed: /usr/local/etc/xray/00_log.json'
-        echo 'installed: /usr/local/etc/xray/01_api.json'
-        echo 'installed: /usr/local/etc/xray/02_dns.json'
-        echo 'installed: /usr/local/etc/xray/03_routing.json'
-        echo 'installed: /usr/local/etc/xray/04_policy.json'
-        echo 'installed: /usr/local/etc/xray/05_inbounds.json'
-        echo 'installed: /usr/local/etc/xray/06_outbounds.json'
-        echo 'installed: /usr/local/etc/xray/07_transport.json'
-        echo 'installed: /usr/local/etc/xray/08_stats.json'
-        echo 'installed: /usr/local/etc/xray/09_reverse.json'
+        echo 'installed: /usr/local/etc/XrayR/00_log.json'
+        echo 'installed: /usr/local/etc/XrayR/01_api.json'
+        echo 'installed: /usr/local/etc/XrayR/02_dns.json'
+        echo 'installed: /usr/local/etc/XrayR/03_routing.json'
+        echo 'installed: /usr/local/etc/XrayR/04_policy.json'
+        echo 'installed: /usr/local/etc/XrayR/05_inbounds.json'
+        echo 'installed: /usr/local/etc/XrayR/06_outbounds.json'
+        echo 'installed: /usr/local/etc/XrayR/07_transport.json'
+        echo 'installed: /usr/local/etc/XrayR/08_stats.json'
+        echo 'installed: /usr/local/etc/XrayR/09_reverse.json'
     fi
     if [ "$LOG" -eq '1' ]; then
-        echo 'installed: /var/log/xray/'
+        echo 'installed: /var/log/XrayR/'
     fi
     if [ "$OPENRC" -eq '1' ]; then
-        echo 'installed: /etc/init.d/xray'
+        echo 'installed: /etc/init.d/XrayR'
     fi
     rm -r "$TMP_DIRECTORY"
     echo "removed: $TMP_DIRECTORY"
     echo "You may need to execute a command to remove dependent software: apk del curl unzip"
     if [ "$XRAY_RUNNING" -eq '1' ]; then
-        rc-service xray start
+        rc-service XrayR start
     else
-        echo 'Please execute the command: rc-update add xray; rc-service xray start'
+        echo 'Please execute the command: rc-update add XrayR; rc-service XrayR start'
     fi
-    echo "info: Xray is installed."
+    echo "info: XrayR is installed."
 }
 
 main() {
